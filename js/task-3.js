824e9e04c39307250f049b1d16589d2551e071ac
WarkezD <warkezd@gmail.com>
2024-07-20 18:35:02 +0200

    up

--- js/task-3.js
+++ js/task-3.js
@@ -1,25 +1,25 @@
 class StringBuilder {
-
+    #value;
     constructor(initialValue) {
-        this.value = initialValue;
+        this.#value = initialValue;
     }
 
     getValue() {
-        return this.value
+        return this.#value
     }
 
     padEnd(str) {
-        this.value += str;
+        this.#value += str;
         return this;
     }
 
     padStart(str) {
-    this.value = str + this.value;
+    this.#value = str + this.#value;
     return this;
     }
     
     padBoth(str) {
-    this.value = str + this.value + str;
+    this.#value = str + this.#value + str;
     return this;
     }
     
